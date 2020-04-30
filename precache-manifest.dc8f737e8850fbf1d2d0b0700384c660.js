ee55b04641ed2858b4bf57f2ff2080f943f56641
Gonzalo Almada <galmada@gmail.com>
2020-04-30 01:56:56 -0300

    Updates

---
+++ precache-manifest.dc8f737e8850fbf1d2d0b0700384c660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb3d6c355f0922fbb8580dc55556596",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0cc7df8f51717f1f0071",
+    "url": "/monster-rolodex/static/css/main.b19c03e8.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc7df8f51717f1f0071",
+    "url": "/monster-rolodex/static/js/main.96cdffd7.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
